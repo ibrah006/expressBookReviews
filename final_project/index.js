e8a541ffb13417e9b12b502edf421ef5cda417f5
Ibrahim <ibrahimmn006@gmail.com>
2024-10-01 05:02:18 -0400

    Uptill Task 8 comleted but yet to be tested (task 8 only)

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,22 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+
+    if (req.session.authorization) {
+        const token = req.session.authorization["token"];
+
+        jwt.verify(token, "access", (err, user)=> {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                res.status(400).json({message: "User is not authenticated!"});
+            }
+        })
+    } else {
+        res.status(400).json({mesage: "User is not logged in."})
+    }
+
 });
  
 const PORT =5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,18 +11,60 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let filteredUsers = users.filter(user=> user.username === usernmae && user.password === password);
+
+    return filteredUsers.length>0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || password) {
+    return res.status(404).json({message: "Failed to login!"});
+  }
+
+  if (authenticatedUser(username, password)) {
+    token = jwt.sign({
+        data: password
+    }, "access", { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+        token, username
+    };
+
+    return res.status(200).json({message: "Succeessfully logged in!"});
+  } else {
+    return res.status(404).json({message: "Invalid login: check Username and Password and try again."})
+  }
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = Number(req.params.isbn);
+  const review = req.body.review;
+
+  const username = req.session.authorization["username"];
+
+  const bookTitle = books[isbn].title;
+
+  if (review && bookTitle) {
+    const bookReviewByUser = books[isbn].review["username"];
+
+    books[isbn].review[username] = review;
+
+    return res.status(200).json({
+        message: (bookReviewByUser? "Succcessfully made changes" : "Successfully added review") +  ` for book ${bookTitle}.`
+    })
+  } else {
+    return res.status(400).json({message: "Failed to add/modify review: please provided a review body."})
+  }
 });
 
 module.exports.authenticated = regd_users;
